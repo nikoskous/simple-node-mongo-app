2853f6c7711e9b646161df34f3499d1f5231abe4
Nikos Kousathanas <nikolaos.kousathanas@gmail.com>
2020-11-12 01:03:44 +0200

    changes

--- src/index.js
+++ src/index.js
@@ -17,21 +17,21 @@ mongoose
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
-const Item = require('./models/item');
+const Product = require('./models/product');
 
 app.get('/', (req, res) => {
-  Item.find()
-    .then(items => res.render('index', { items }))
-    .catch(err => res.status(404).json({ msg: 'No items found' }));
+  Product.find()
+    .then(products => res.render('index', { products }))
+    .catch(err => res.status(404).json({ msg: 'No products found' }));
 });
 
-app.post('/item/add', (req, res) => {
-  const newItem = new Item({
+app.post('/product/add', (req, res) => {
+  const newProduct = new Product({
     name: req.body.name,
     quantity: req.body.quantity
   });
 
-  newItem.save().then(item => res.redirect('/'));
+  newProduct.save().then(product => res.redirect('/'));
 });
 
 const port = 3000;
