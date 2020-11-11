cb9df2de1f6dd269dcd1cd328fa6ab1dd7e17b67
Nikos Kousathanas <nikolaos.kousathanas@gmail.com>
2020-11-11 17:17:11 +0200

    δφγ

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-// app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 
 // Connect to MongoDB
 mongoose
